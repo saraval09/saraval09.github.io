f2be3bfa768c23709c1a02574ca25ab87ce817c1
Sara <saraval09@gmail.com>
2016-09-29 19:02:58 -0400

    Revised Google Maps
    Revised Google Maps

--- js/main.js
+++ js/main.js
@@ -35,39 +35,23 @@ app.controller('PageCtrl', function (/* $scope, $location, $http */) {
         interval: 5000
     });
 
-    // Activates Tooltips for Social Links
-    $('.tooltip-social').tooltip({
-        selector: "a[data-toggle=tooltip]"
-    })
+
 });
 
+/**
+ * Google Map
+ */
 
-$(document).ready(function() {
-    $('#mapForm').change(function() {
 
-        var selectedCities = $('#mapForm option:selected').val();
-        if (selectedCities == 'ALL'){
-            $('a.dot').slideDown(1000);
-        }else{
-            $('a.dot[cities = "'+selectedCities+'"]').show(1000);
-            $('a.dot[cities != "'+selectedCities+'"]').hide(1000);
-        }
+var map;
 
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: -34.397,
+            lng: 150.644
+        },
+        zoom: 8
     });
-
-    $('a.dot').click(function(){
-        $('a.dot').removeClass('selected');
-        $(this).addClass('selected');
-
-
-        var cities = '.city_detail#' + $(this).attr('cities');
-        var htmlCode = $(cities).html();
-
-        $('.detail_container').fadeOut(500, function(){
-            $('.detail_container .city_detail').html(htmlCode);
-            $('.detail_container').fadeIn(500);
-        });
-    });
-});
-
+}
 
