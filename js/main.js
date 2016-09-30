90d71245e8ab34e6a72520c381b005281cd2a154
Sara <saraval09@gmail.com>
2016-09-29 21:10:34 -0400

    Google Maps
    Trying to get the map to work

--- js/main.js
+++ js/main.js
@@ -87,14 +87,9 @@ function updateThumbnails(){
 
 /**Google Map**/
 var map;
-
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-            lat: -34.397,
-            lng: 150.644
-        },
+        center: {lat: -34.397, lng: 150.644},
         zoom: 8
     });
 }
-
