7bfd93223cf021c7cd725ea3588135ac44e2733b
Sara <saraval09@gmail.com>
2016-10-16 20:56:49 -0400

    Images
    images

--- js/main.js
+++ js/main.js
@@ -20,12 +20,41 @@ app.config(['$routeProvider', function ($routeProvider) {
 
 /**Images**/
 
+    $(document).ready(function(){
+        $("a[rel^='prettyPhoto']").prettyPhoto();
+    });
 
+$(document).ready(function(){
+    $("area[rel^='prettyPhoto']").prettyPhoto();
 
+    $(".gallery:first a[rel^='prettyPhoto']").prettyPhoto({animation_speed:'normal',theme:'light_square',slideshow:3000, autoplay_slideshow: true});
+    $(".gallery:gt(0) a[rel^='prettyPhoto']").prettyPhoto({animation_speed:'fast',slideshow:10000, hideflash: true});
+
+    $("#custom_content a[rel^='prettyPhoto']:first").prettyPhoto({
+        custom_markup: '<div id="map_canvas" style="width:260px; height:265px"></div>',
+        changepicturecallback: function(){ initialize(); }
+    });
+
+    $("#custom_content a[rel^='prettyPhoto']:last").prettyPhoto({
+        custom_markup: '<div id="bsap_1259344" class="bsarocks bsap_d49a0984d0f377271ccbf01a33f2b6d6"></div><div id="bsap_1237859" class="bsarocks bsap_d49a0984d0f377271ccbf01a33f2b6d6" style="height:260px"></div><div id="bsap_1251710" class="bsarocks bsap_d49a0984d0f377271ccbf01a33f2b6d6"></div>',
+        changepicturecallback: function(){ _bsap.exec(); }
+    });
+});
 
 
 /**Google Map**/
-function initMap() {
+function initialize() {
+    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    var myOptions = {
+        zoom: 8,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map_canvas"),
+        myOptions);
+}
+
+/*function initMap() {
     function init_map() {
         var myOptions = {
             zoom:10,center:new google.maps.LatLng(27.044224,-82.23592539999999),mapTypeId: google.maps.MapTypeId.SATELLITE
@@ -50,5 +79,5 @@ function geocodeAddress(geocoder, resultsMap) {
             alert('Geocode was not successful for the following reason: ' + status);
         }
     });
-}
+}*/
 
