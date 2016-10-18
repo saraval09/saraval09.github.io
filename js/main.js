9c3860e9eb1fd5f5860f1bc17cc1e567b1e20af0
Sara <saraval09@gmail.com>
2016-10-18 09:24:58 -0400

    Updated Google Maps
    Updated Maps

--- js/main.js
+++ js/main.js
@@ -46,41 +46,31 @@ jQuery(document).ready(function($) {
 
 
 /**Google Map**/
-function initialize() {
-    var latlng = new google.maps.LatLng(-34.397, 150.644);
-    var myOptions = {
-        zoom: 8,
-        center: latlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map_canvas"),
-        myOptions);
-}
+(function() {
 
-/*function initMap() {
-    function init_map() {
-        var myOptions = {
-            zoom:10,center:new google.maps.LatLng(27.044224,-82.23592539999999),mapTypeId: google.maps.MapTypeId.SATELLITE
-        };
-    }
-    map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);
+    var app = angular.module('gmaps', []);
 
-    marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(27.044224,-82.23592539999999)});
+    app.directive('googleMap', function () {
+        return {
 
-    infowindow = new google.maps.InfoWindow({content:'<strong>X</strong><br>North port<br>'});google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);
+            restrict: 'AE',
 
-function geocodeAddress(geocoder, resultsMap) {
-    var address = document.getElementById('address').value;
-    geocoder.geocode({'address': address}, function(results, status) {
-        if (status === 'OK') {
-            resultsMap.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-                map: resultsMap,
-                position: results[0].geometry.location
-            });
-        } else {
-            alert('Geocode was not successful for the following reason: ' + status);
+            template: '<div id="map"></div>',
+
+            controller: function ($scope) {
+
+                var mapOptions = {
+                    zoom: 15,
+                    center: new google.maps.LatLng(28.602432, -81.200264),
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+
+                $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+            }
         }
+
     });
-}*/
+
+    // Passing google as a dependency into this module
+})(google);
 
