a9e606ab4cd27b0bb300b1a617ae5ed3efff9403
Sara <saraval09@gmail.com>
2016-10-24 22:42:30 -0400

    Nav Bar
    Connecting Nav bar using directives

--- js/main.js
+++ js/main.js
@@ -13,10 +13,28 @@ app.config(['$routeProvider', function ($routeProvider) {
         // Pages
         .when("/bio", {templateUrl: "partials/bio.html"})
         .when("/contact", {templateUrl: "partials/contact.html"})
-        .when("/videos", {templateUrl: "/partials/videos.html"})
         // else 404
         .otherwise("/404", {templateUrl: "partials/404.html"});
 }]);
+
+    /** Footer **/
+    app.directive('footer', function () {
+        return {
+            restrict: 'A',
+            replace: true,
+            templateUrl: "../templates/footer.html"
+        }
+    });
+
+    /** Header **/
+    app.directive('header', function () {
+        return {
+            restrict: 'A',
+            replace: true,
+            scope: {user: '='},
+            templateUrl: "../templates/header.html"
+        }
+    });
 
     /** Images and Videos **/
 
@@ -76,8 +94,8 @@ app.directive('googleMap', function () {
         controller: function ($scope) {
 
             var mapOptions = {
-                zoom: 15,
-                center: new google.maps.LatLng(28.602432, -81.200264),
+                zoom: 10,
+                center: new google.maps.LatLng(41.1725694, -76.8663396),
                 mapTypeId: google.maps.MapTypeId.ROADMAP
             };
 
@@ -88,9 +106,79 @@ app.directive('googleMap', function () {
 });
 })(google);
 
+function initialize() {
+    var mapOptions = {
+        center: {lat: 41.1725694, lng: -76.8663396},
+        zoom: 10,
+        scrollwheel: false
+    };
+    var map = new google.maps.Map(document.getElementById('map'),
+        mapOptions);
 
+    var input = /** @type {HTMLInputElement} */(
+        document.getElementById('pac-input'));
 
+    // Create the autocomplete helper, and associate it with
+    // an HTML text input box.
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.bindTo('bounds', map);
 
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    var infowindow = new google.maps.InfoWindow();
+    var marker = new google.maps.Marker({
+        map: map
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map, marker);
+    });
+
+    // Get the full place details when the user selects a place from the
+    // list of suggestions.
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+        infowindow.close();
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+            return;
+        }
+
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);
+        }
+
+        // Set the position of the marker using the place ID and location.
+        marker.setPlace(/** @type {!google.maps.Place} */ ({
+            placeId: place.place_id,
+            location: place.geometry.location
+        }));
+        marker.setVisible(true);
+
+        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+            'Place ID: ' + place.place_id + '<br>' +
+            place.formatted_address + '</div>');
+        infowindow.open(map, marker);
+    });
+}
+
+var x = document.getElementById("demo");
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude;
+    console.log('This is your location ' + showPosition());
+}
+
+// Run the initialize function when the window has finished loading.
+google.maps.event.addDomListener(window, 'load', initialize);
 
 
 
